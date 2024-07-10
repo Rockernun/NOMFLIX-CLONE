a281460af0cf9da13863042b845e41531adafe14
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2024-07-10 15:25:38 +0900

    Using Styled Components
    헤더(Nav)에는 2개의 열(Col)이 있고, 하나는 Logo + 카테고리(Items, Item), 다른 하나는 검색창, 알림, 유저 등의 정보가 있다.

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,5 +1,56 @@
+import styled from "styled-components";
+
+//  헤더(네비게이션)
+const Nav = styled.nav`
+    width:100%;
+    display:flex;
+    justify-content:space-between;
+    align-items:center;
+    position:fixed;
+    top:0;
+    background-color:red;
+    height:80px;
+    font-size:14px;
+`;
+
+//  Header의 2개의 열
+const Col = styled.div`
+    display:flex;
+    align-items:center;
+`;
+
+//  로고
+const Logo = styled.svg`
+    margin-right:50px;
+`;
+
+
+//  Home, Tv Shows item 담아둘 컴포넌트
+const Items = styled.ul`
+    display:flex;
+    align-items:center;
+`;
+
+//  Home, Tv Shows item 컴포넌트
+const Item = styled.li`
+    margin-right:20px;
+`;
+
 function Header() {
-    return null;
+    return (
+        <Nav>
+            <Col>
+                <Logo />
+                <Items>
+                    <Item>Home</Item>
+                    <Item>Tv Shows</Item>
+                </Items>
+            </Col>
+            <Col>
+            <button>search</button>
+            </Col>
+        </Nav>
+    );
 }
 
 export default Header;
